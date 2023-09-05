87e151236fbae65a3a0cb11861fcc31d33c34f28
mark <mdnpags@gmail.com>
2023-09-05 11:04:28 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,2 @@
+var characters = prompt("Tell me about yourself");
+alert("You have written " + characters.length + " characters, you only have " + (150-characters.length) + " remaining characters.")
